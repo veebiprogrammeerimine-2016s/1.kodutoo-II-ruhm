199fd23bdf4e3b8e43897e12ba4db7563999f4e6
karlbork <karlbork@greeny.cs.tlu.ee>
2016-09-18 18:01:14 +0300

    updated login

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+ Mõtlesin teha broneeringulehe telkimis- ja haagissuvilate platside jaoks. Idee seisneb siis selles, et mul on kujuteldav krunt maad, 
+ mille olen jaganud telkimis- ja haagissuvilate platsideks. Veebilehel peaks ideaalis saama broneerida platse millel on erinevad omadused erinevate hindadega (vaiksem koht, lastega pered, ...), juurde käivad teenused nagu elekter jne.
+ Osadele platsidele tekivad järjekorrad, osad saadavad koguaeg, ehk siis ka ootejärjekorrad jm.
+ 
+ 
  <?php
 	
 	//GET ja POSTi muutujad
@@ -7,6 +12,11 @@
  
 	$signupEmailError = "";
 	$signupPasswordError = "";
+	$nameError = "";
+	$dateOfBirthError = "";
+	$addressError = "";
+	$phoneNumberError = "";
+	
 	
 	//on üldse olemas selline muutuja
 	if( isset($_POST["signupEmail"] ) ){
@@ -15,34 +25,70 @@
 		//kas on tühi
 		if( empty($_POST["signupEmail"] ) ){
 			
-			$signupEmailError = "See väli on kohustuslik";
-			
-		}
-	
+			$signupEmailError = "See väli on kohustuslik";		
+		}	
 	}
 		
  
- if( isset($_POST["signupPassword"] ) ){
-		
-		if( empty($_POST["signupPassword"] ) ){
-			
-			$signupPasswordError = "Parool on kohustuslik";
-			
-		} else {
-			
+ if( isset($_POST["signupPassword"] ) ){	
+		if( empty($_POST["signupPassword"] ) ){		
+			$signupPasswordError = "Parool on kohustuslik";	
+		} else {	
 			//siia jõuan siis kui parool oli olemas,
-			//parool ei olnud tühi -empty
-			
-			if (strlen($_POST["signupPassword"]) < 8 ) {
-				
-				$signupPasswordError = "Parool peab olema vähemalt 8 tähemärki";
-				
-			}
-			
+			//parool ei olnud tühi -empty	
+			if (strlen($_POST["signupPassword"]) < 8 ) {	
+				$signupPasswordError = "Parool peab olema vähemalt 8 tähemärki";		
+			}		
 		}
-	
 	}
  
+ if (isset ($_POST["firstName"])){
+		if (empty($_POST["firstName"])){
+			$nameError = "Palun sisestage oma täisnimi";
+		}
+ }
+ 
+ if (isset ($_POST["lastName"])){
+		if (empty($_POST["lastName"])){
+			$nameError = "Palun sisestage oma täisnimi";
+		}		
+ }
+ 
+ if (isset ($_POST["dateDay"])){
+		if (empty($_POST["dateDay"])){
+			$dateOfBirthError = "Palun sisestage sünniaeg";
+		}
+ }
+ 
+ if (isset ($_POST["dateMonth"])){
+		if (empty($_POST["dateMonth"])){
+			$dateOfBirthError = "Palun sisestage sünniaeg";
+		}
+ }
+ 
+ if (isset ($_POST["dateYear"])){
+		if (empty($_POST["dateYear"])){
+			$dateOfBirthError = "Palun sisestage sünniaeg";
+		}
+ }
+ 
+ if (isset ($_POST["country"])){
+		if (empty($_POST["country"])){
+			$addressError = "Palun sisestage oma aadress";
+		}
+ }
+ 
+ if (isset ($_POST["address"])){
+		if (empty($_POST["address"])){
+			$addressError = "Palun sisestage oma aadress";
+		}
+ }
+ 
+ if (isset ($_POST["phoneNumber"])){
+		if (empty($_POST["phoneNumber"])){
+			$phoneNumberError = "Palun sisestage oma telefoninumber";
+		}
+ }
  
  ?>
  
@@ -73,14 +119,40 @@
 	
 	<h1>Loo kasutaja</h1>
 	<form method="POST">
-			<label>E-post</label><br>
-			<input name="signupEmail" type="text"> <?php echo $signupEmailError; ?>
+			
+			<input name="signupEmail" type="text" placeholder="E-mail"> <?php echo $signupEmailError; ?>
 			<br><br>
 			
 			<input name="signupPassword" placeholder="Parool" type="password"> <?php echo $signupPasswordError; ?>
 			<br><br>
 			
+			<label>Nimi</label><br>
+			<input name="firstName" placeholder="Eesnimi" type="text">
+			<input name="lastName" placeholder="Perekonnanimi" type="text"> <?php echo $nameError; ?>
+			<br><br>
+			
+			<label>Sugu</label><br>
+			<input name="gender" type="radio" value="male"> Mees
+			<input name="gender" type="radio" value="female"> Naine
+			<br><br>
+			
+			<label>Sünniaeg</label><br>
+			<input type="number" name="dateDay" placeholder="Päev">
+			<input type="number" name="dateMonth" placeholder="Kuu">
+			<input type="number" name="dateYear" placeholder="Aasta"> <?php echo $dateOfBirthError; ?>
+			<br><br>
+			
+			<label>Aadress</label><br>
+			<input type="text" name="country" placeholder="Riik">
+			<input type="text" name="address" placeholder="Aadress"> <?php echo $addressError; ?>
+			<br><br>
+			
+			<label>Kontakttelefon</label><br>
+			<input type="text" name="phoneNumber"> <?php echo $phoneNumberError; ?>
+			<br><br>
+			
 			<input type="submit" value="Loo kasutaja">
+	
 	
 	</form>
 	
