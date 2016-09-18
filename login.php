9a40de14c2bc0344b07d90257a907f91a728c3c6
karojyrg <karojyrg@greeny.cs.tlu.ee>
2016-09-18 19:53:20 +0300

    login

--- login.php
+++ login.php
@@ -57,18 +57,38 @@ $singupPasswordError = "Parool peab olema v�hemalt 8 tm pikk ";
 		<br>
 		
 		<input name="signupEmail" type="text"> <?php echo $signupEmailError; ?>
+		
 		<br><br>
 		
 		<input type="password" name="signupPassword" placeholder="Parool">
+		
 		<br><br>
-                <br><br>
-		 <label>Password</label>
-		 <input name="loginPassword" type="password" <?php echo $signupPasswordError ?>
-  
-  
- 		 <br><br>
+		
+        <form>
+		Sugu:<br>
+		<input type="radio" name="sugu" value="mees" checked> Mees<br>
+		<input type="radio" name="sugu" value="naine"> Naine<br>
+		<input type="radio" name="sugu" value="muu"> Muu
+		</form>
+
+		<br>
+		
+		<form>
+		Sünnipäev:<br>
+		<input type="date" name="sünnipäev" max="2006-12-31">
+		<br><br>
+		
+		<form>
+		Keel:<br>
+		<select name="keel">
+		<option value="eesti keel">eesti keel</option>
+		<option value="vene keel">vene keel</option>
+		<option value="inglise keel">inglise keel</option>
+		</select>
+		<br><br>
 		
 		<input type="submit" value="Loo kasutaja">
+		
 		
 		
 	</form>
