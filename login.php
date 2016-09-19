d9860a4d8cb21683acbd6da8cb42b8eb5182b1e9
anuxyo <anuusadam@gmail.com>
2016-09-19 21:34:18 +0300

    Update login.php

--- login.php
+++ login.php
@@ -29,7 +29,7 @@
 
 			if( empty($_POST["signupPassword"])){
 			
-				$signupPasswordError = "Parool on kohustuslik";
+				$signupPasswordError = "See väli on kohustuslik";
 				
 			}else{
 				
@@ -48,6 +48,19 @@
 
 ?>
 
+<?php
+
+	$signupAastaError = "";
+	
+	if(isset($_POST["signupAasta"])){
+		
+		if(empty($_POST["signupAasta"])){
+			
+			$signupAastaError = "See väli on kohustuslik";
+		
+		}
+	}
+?>
 
 
 
@@ -60,10 +73,8 @@
 
 	<h1>Logi sisse</h1>
 	<form method="POST">
-		<label>E-post</label>
-		<br>
 		
-		<input name="LoginEmail" type="text">
+		<input name="LoginEmail" type="text" placeholder="E-post">
 		<br><br>
 		
 		<input type="password" name="LoginPassword" placeholder="Parool">
@@ -75,22 +86,85 @@
 		
 	</form>
 	
-	<h1>Loo kasuataja</h1>
+	<h1>Loo kasutaja</h1>
 	<form method="POST">
 		<label>E-post</label>
 		<br>
 		
 		<input name="signupEmail" type="text"> <?php echo $signupEmailError; ?>
+		<br>
+		
+		<label>Parool</label>
+		<br>
+		<input type="password" name="signupPassword"> <?php echo $signupPasswordError; ?>
 		<br><br>
 		
-		<input type="password" name="signupPassword" placeholder="Parool"> <?php echo $signupPasswordError; ?>
-		<br><br>
-		
-		<input type="submit" value="Loo kasutaja">
-		
+	<h2>Sugu</h2>
+	  <input type="radio" name="Sugu" value="Mees"> Mees<br>
+	  <input type="radio" name="Sugu" value="Naine"> Naine<br>
+	  <input type="radio" name="Sugu" value="Muu"> Muu
+	<br><br>
 	
-		
-	</form>
+	<h2>Sünnipäev</h2>
+	<h3>Kuu</h3>
+	<select>
+		<option value="Jaanuar">Jaanuar</option>
+		<option value="Veebruar">Veebruar</option>
+		<option value="Märts">Märts</option>
+		<option value="Aprill">Aprill</option>
+		<option value="Mai">Mai</option>
+		<option value="Juuni">Juuni</option>
+		<option value="Juuli">Juuli</option>
+		<option value="August">August</option>
+		<option value="September">September</option>
+		<option value="Oktoober">Oktoober</option>
+		<option value="November">November</option>
+		<option value="Detsember">Detsember</option>
+	</select>
+	<br>
+	
+		<h3>Päev</h3>
+	<select>
+	<option value='1'>1</option>
+	<option value='2'>2</option>
+	<option value='3'>3</option>
+	<option value='4'>4</option>
+	<option value='5'>5</option>
+	<option value='6'>6</option>
+	<option value='7'>7</option>
+	<option value='8'>8</option>
+	<option value='9'>9</option>
+	<option value='10'>10</option>
+	<option value='11'>11</option>
+	<option value='12'>12</option>
+	<option value='13'>13</option>
+	<option value='14'>14</option>
+	<option value='15'>15</option>
+	<option value='16'>16</option>
+	<option value='17'>17</option>
+	<option value='18'>18</option>
+	<option value='19'>19</option>
+	<option value='20'>20</option>
+	<option value='21'>21</option>
+	<option value='22'>22</option>
+	<option value='23'>23</option>
+	<option value='24'>24</option>
+	<option value='25'>25</option>
+	<option value='26'>26</option>
+	<option value='27'>27</option>
+	<option value='28'>28</option>
+	<option value='29'>29</option>
+	<option value='30'>30</option>
+	<option value='31'>31</option>
+	</select>
 
+		<h3>Aasta</h3>
+		<input name="signupAasta" type="text"> <?php echo $signupAastaError; ?>
+		<br><br>
+		
+	<input type="submit" value="Loo kasutaja">
+
+</form>
+	
 </body>
 </html>
