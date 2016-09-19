cb9452904cd62493b46fb00e00bc7fc17efc6e59
markkoll <markkoll@greeny.cs.tlu.ee>
2016-09-19 13:55:10 +0300

    Added validation and mandatory fields.

--- login.php
+++ login.php
@@ -5,6 +5,9 @@
 	//var_dump($_POST);
 	
 	$signupEmailError = "";
+	$loginPasswordError = "";
+	$signupSecurityQuestionError = "";
+	$signupSecurityAnswerError = "";
 	
 	// on üldse olemas selline muutja
 	if( isset( $_POST["signupEmail"] ) ){
@@ -13,12 +16,41 @@
 		//kas on tühi
 		if( empty( $_POST["signupEmail"] ) ){
 			
-			$signupEmailError = "See väli on kohustuslik";
-			
+			$signupEmailError = "See väli on kohustuslik";}
 		}
+
+	
+	
+	if( isset( $_POST["loginPassword"] ) ){
 		
-	}
+
+		if( empty( $_POST["loginPassword"] ) ){
+			
+			$signupPasswordError = "See väli on kohustuslik";}
+		}
+
+	
+	
+	if( isset( $_POST["signupSecurityQuestion"] ) ){
+		
+
+		if( empty( $_POST["signupSecurityQuestion"] ) ){
+			
+			$signupSecurityQuestionError = "See väli on kohustuslik";}
+		}
+
+
+		
+	if( isset( $_POST["signupSecurityAnswer"] ) ){
+		
+
+		if( empty( $_POST["signupSecurityAnswer"] ) ){
+			
+			$signupSecurityAnswerError = "See väli on kohustuslik";}
+		}
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,7 +67,7 @@
 		<input name="loginEmail" type="text">
 		<br><br>
 		
-		<input type="password" name="loginPassword" placeholder="Parool">
+		<input type="password" name="loginPassword" placeholder="Parool"><?php echo $loginPasswordError; ?>
 		<br><br>
 		
 		<input type="submit" value="Logi sisse">
@@ -47,8 +79,7 @@
 	<h1>Loo kasutaja</h1>
 	<form method="POST">
 		
-		<label>E-post</label>
-		<br>
+		<label>E-post</label><br>
 		
 		<input name="signupEmail" type="text"> <?php echo $signupEmailError; ?><br><br>
 		
@@ -58,8 +89,8 @@
 		
 		<input name="signupLastName" placeholder="Perekonnanimi"><br><br>
 
-		<input name="signupSecurityQuestion" placeholder="Turvaküsimus"><br>
-		<input name="signupSecurityAnswer" type="password" placeholder="Vastus"><br><br>
+		<input name="signupSecurityQuestion" placeholder="Turvaküsimus"><?php echo $signupSecurityQuestionError; ?><br>
+		<input name="signupSecurityAnswer" type="password" placeholder="Vastus"><?php echo $signupSecurityAnswerError; ?><br><br>
 		
 		<input type="radio" name="gender" value="Mees"> Mees<br>
 		<input type="radio" name="gender" value="Ńaine"> Naine<br>
